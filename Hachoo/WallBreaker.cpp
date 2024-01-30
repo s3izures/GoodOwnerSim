a6539d647e3a19aff98bd24d9fb37acaf6cbf504
S3izures <seizuresthree@gmail.com>
2024-01-30 08:25:01 -0800

    Add project files.

---
+++ Hachoo/WallBreaker.cpp
@@ -0,0 +1,27 @@
+#include "WallBreaker.h"
+#include "raylib.h"
+
+void WallBreaker::Main()
+{
+
+}
+
+void WallBreaker::Start()
+{
+
+}
+
+void WallBreaker::EvalCurFrame()
+{
+
+}
+
+void WallBreaker::DrawCurFrame()
+{
+
+}
+
+void WallBreaker::Update()
+{
+
+}
---
+++ Hachoo/WallBreaker.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "raylib.h"
+#ifndef WALLBREAKER_H
+#define WALLBREAKER_H
+
+class WallBreaker
+{
+public:
+
+	//FUNCTIONS
+	void Main();
+	void Start();
+	void EvalCurFrame();
+	void DrawCurFrame();
+	void Update();
+};
+
+#endif
+
