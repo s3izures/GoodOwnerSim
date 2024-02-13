87ad72f3b71b7e9a68d9b084abe2310ae6918372
S3izures <seizuresthree@gmail.com>
2024-02-13 08:28:51 -0800

    Started project.

---
+++ Prrp/Prrp/GuessGame.cpp
@@ -0,0 +1,1 @@
+#include "GuessGame.h"
---
+++ Prrp/Prrp/GuessGame.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <raylib.h>
+
+class MemoryGame
+{
+public:
+
+	//FUNCTIONS
+	void Main();
+	void Start();
+	void Update();
+	void EvalFrame();
+	void DrawFrame();
+};
+
