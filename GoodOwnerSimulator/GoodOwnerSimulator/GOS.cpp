25f53d69700f7caeea00052dc1ca3b6f4725809e
eN <seizuresthree@gmail.com>
2024-03-08 19:10:27 -0800

    Finished a basic gameover screen, implemented pet and play.

--- GoodOwnerSimulator/GoodOwnerSimulator/GOS.cpp
+++ GoodOwnerSimulator/GoodOwnerSimulator/GOS.cpp
@@ -4,7 +4,7 @@
 
 void GOS::Main()
 {
-	InitWindow(screenX, screenY, "Good Owner Simulator");
+	InitWindow(screenX, screenY, "");
 	SetTargetFPS(60);
 
 	Start();
@@ -13,7 +13,7 @@ void GOS::Main()
 	{
 		BeginDrawing();
 
-		ClearBackground(DARKGRAY);
+		ClearBackground(backgroundCol);
 		Update();
 
 		EndDrawing();
@@ -24,21 +24,28 @@ void GOS::Main()
 
 void GOS::Start()
 {
+	SetWindowTitle("Good Owner Simulator");
 	pet = Pet();
+
+	//Timer reset
+	stareTimer = 0;
+	playTimer = 0;
+	pettingTimer = 0;
+
+	//Background reset
+	backgroundCol = LIGHTGRAY;
 }
 
 void GOS::Update()
 {
 	EvalFrame();
 	DrawFrame();
-
-	//use GetTime() for timers
-	//use GetFrameTime() for animations?
 }
 
 void GOS::EvalFrame()
 {
 
+	PetInteraction();
 }
 
 void GOS::DrawFrame()
@@ -46,7 +53,30 @@ void GOS::DrawFrame()
 	if (!gameOver)
 	{
 		pet.Draw();
-		pet.LookAtMouse();
+	}
+	else
+	{
+		backgroundCol = BLACK;
+		int fontSize = 50;
+		int fontSizeSub = 20;
+
+		SetWindowTitle("YOU'RE A BAD OWNER.");
+
+		//TEXT
+		const char* gameover= "GAME OVER";
+		DrawText(gameover,
+			screenX / 2 - MeasureText(gameover, fontSize) / 2,
+			screenY / 2 - fontSize,
+			fontSize,
+			WHITE);
+
+		//Do custom gameover screens
+		const char* subtitle = "THIS IS ALL YOUR FAULT THIS IS ALL YOUR FAULT THIS IS ALL YOUR FAULT";
+		DrawText(subtitle,
+			screenX / 2 - MeasureText(subtitle, fontSizeSub) / 2,
+			screenY / 2 + fontSize / 2,
+			fontSizeSub,
+			RED);
 	}
 }
 
@@ -54,7 +84,137 @@ void GOS::DrawFrame()
 
 #pragma region Additional Fucntions
 
+void GOS::PetInteraction()
+{
+	//Play
+	if (!petBusy && IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+	{
+		Vector2 mousePoint = GetMousePosition();
+		pet.LookAt(mousePoint);
 
+		stareTimer += GetFrameTime();
+
+		if (stareTimer >= 4)
+		{
+			pet.Move(mousePoint);
+			playTimer += GetFrameTime();
+
+			if (playTimer >= 5 && canPlay)
+			{
+				pet.play++;
+				pet.energy--;
+				cout << pet.play;
+				canPlay = false;
+			}
+		}
+	}
+	else
+	{
+		canPlay = true;
+		stareTimer = 0;
+		playTimer = 0;
+		pet.emote = Neutral;
+		pet.mood = Default;
+		pet.ResetEyes();
+	}
+
+	//Petting
+	if (!IsMouseButtonDown(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(GetMousePosition(), pet.body))
+	{
+		petBusy = true;
+
+		pet.ResetEyes();
+		pettingTimer += GetFrameTime();
+
+		if (canPet)
+		{
+			pet.emote = ClosedDown; //not petted but petting
+		}
+		else
+		{
+			pet.mood = Happy;
+			pet.emote = ClosedUp; //petted
+		}
+
+		if (pettingTimer >= 4 && canPet)
+		{
+			pet.love++;
+			cout << pet.love;
+			canPet = false;
+		}
+	}
+	else
+	{
+		canPet = true;
+		pettingTimer = 0;
+		pet.mood = Default;
+		pet.emote = Neutral;
+		petBusy = false;
+	}
+}
+
+int GOS::PetCheck()
+{
+	if (pet.fullness <= 0)
+	{
+		gameOver = true;
+		return 1; //died of hunger
+	}
+	if (pet.energy <= 0)
+	{
+		gameOver = true;
+		return 2; //died of exhaustion
+	}
+	if (pet.play <= 0)
+	{
+		gameOver = true;
+		return 3; //died of boredom
+	}
+	if (pet.love <= 0)
+	{
+		gameOver = true;
+		return 4; //died of sadness
+	}
+
+	if (pet.fullness >= 420)
+	{
+		gameOver = true;
+		return 5; //died of overfeeding
+	}
+	if (pet.play <= 21)
+	{
+		gameOver = true;
+		return 6; //died of excitement
+	}
+	if (pet.love <= 69)
+	{
+		gameOver = true;
+		return 7; //died of too much love
+	}
+
+	if (pet.fullness <= 5)
+	{
+		pet.emote = Squint;
+		pet.mood = Hungry;
+	}
+	else if (pet.energy <= 5)
+	{
+		pet.emote = Squint;
+		pet.mood = Tired;
+	}
+	else if (pet.play <= 5)
+	{
+		pet.emote = Squint;
+		pet.mood = Sad;
+	}
+	else if (pet.love <= 5)
+	{
+		pet.emote = ClosedDown;
+		pet.mood = Sad;
+	}
+
+	return 0; //Alive
+}
 
 #pragma endregion
 
--- GoodOwnerSimulator/GoodOwnerSimulator/GOS.h
+++ GoodOwnerSimulator/GoodOwnerSimulator/GOS.h
@@ -18,35 +18,77 @@ private:
 	static const int screenY = 500;
 	static const int screenX = 800;
 	bool gameOver = false;
+	Color backgroundCol = LIGHTGRAY;
 
-#pragma region Structs
+	bool petBusy = false;
+	float stareTimer;
+	float playTimer;
+	float pettingTimer;
+	bool canPet = true;
+	bool canPlay = true;
+	bool isMoving = false;
+
+	void PetInteraction();
+	int PetCheck();
+
+	enum Mood {
+		Default,
+		Happy,
+		Sad,
+		Hungry,
+		Tired
+	};
+
+	enum Expression {
+		Neutral,
+		ClosedDown,
+		ClosedUp,
+		Squint,
+	};
+
+#pragma region Objects
 
 	typedef struct Pet {
+		//Stats
+		Mood mood;
+		Expression emote;
+		int fullness;
+		int energy;
+		int play;
+		int love;
+
 		//Appearance
-		//Shapes
-			Rectangle body;
-			Rectangle leftEar, rightEar;
-			Rectangle leftEye, rightEye;
-			Rectangle tail;
-		//Size
-			float bodySize = 100;
-			Vector2 earSize = { 40, 60 };
-			float eyeSize = 17;
-			float tailSize = 30;
-		//Colors
-			Color bodyColor = WHITE;
-			Color earColor = BLACK;
-			Color eyeColor = BLUE;
-			Color tailColor = BLACK;
+			//Shapes
+				Rectangle body;
+				Rectangle leftEar, rightEar;
+				Rectangle leftEye, rightEye;
+				Rectangle tail;
+			//Size
+				float bodySize = 100;
+				Vector2 earSize = { 40, 60 };
+				float eyeSize = 17;
+				float tailSize = 30;
+			//Colors
+				Color bodyColor = WHITE;
+				Color earColor = BLACK;
+				Color tailColor = BLACK;
+				Color eyeColor;
 
 		//Variables
 		Vector2 position;
-		Vector2 speed;
+		Vector2 speed = { 4, 3 };
 		Vector4 eyeAnchor;
 
 		//Functions
 		Pet() //Initialization
 		{
+			mood = Default;
+			emote = Neutral;
+			fullness = 10;
+			energy = 10;
+			play = 10;
+			love = 10;
+
 			position = Vector2{ screenX / 2,screenY / 2 };
 
 			body = Rectangle	{ 
@@ -93,8 +135,6 @@ private:
 								eyeSize,
 								eyeSize
 			};
-
-			speed = Vector2{ 0,0 };
 		}
 		void Draw()
 		{
@@ -104,67 +144,201 @@ private:
 			DrawRectangleRec(leftEar, earColor);
 			DrawRectangleRec(rightEar, earColor);
 
+			switch (mood)
+			{
+			case Default:
+				eyeColor = BLACK;
+				break;
+			case Happy:
+				eyeColor = PINK;
+				break;
+			case Sad:
+				eyeColor = DARKBLUE;
+				break;
+			case Hungry:
+				eyeColor = BROWN;
+				break;
+			case Tired:
+				eyeColor = GRAY;
+				break;
+			default:
+				eyeColor = BLACK;
+				break;
+			}
+
 			DrawRectangleRec(leftEye, eyeColor);
 			DrawRectangleRec(rightEye, eyeColor);
+
+			switch (emote)
+			{
+			case Neutral:
+				break;
+			case ClosedDown:
+				DrawRectangle(leftEye.x + 2, leftEye.y, leftEye.width - 4, leftEye.height - 2, bodyColor);
+				DrawRectangle(rightEye.x + 2, rightEye.y, rightEye.width - 4, rightEye.height - 2, bodyColor);
+				break;
+			case ClosedUp:
+				DrawRectangle(leftEye.x + 2, leftEye.y + 4, leftEye.width - 4, leftEye.height - 2, bodyColor);
+				DrawRectangle(rightEye.x + 2, rightEye.y + 4, rightEye.width - 4, rightEye.height - 2, bodyColor);
+				break;
+			case Squint:
+				DrawRectangle(leftEye.x, leftEye.y, leftEye.width, 5, bodyColor);
+				DrawRectangle(leftEye.x, leftEye.y + leftEye.height/2 + 5, leftEye.width, 5, bodyColor);
+				DrawRectangle(rightEye.x, rightEye.y, rightEye.width, 5, bodyColor);
+				DrawRectangle(rightEye.x, rightEye.y + rightEye.height/2 + 5, rightEye.width, 5, bodyColor);
+				break;
+			default:
+				break;
+			}
 		}
-		void LookAtMouse() 
+		void LookAt(Vector2 point) 
 		{
-			Vector2 mousePoint = GetMousePosition();
-			Vector2 leftEyeOG = Vector2{ eyeAnchor.x,eyeAnchor.y };
-			Vector2 rightEyeOG = Vector2{ eyeAnchor.z,eyeAnchor.w };
-			float eyeMovementRadius = 3; //Area around eye
+			float eyeMovementRadius = 2; //Area around eye
 
 			//LEFT EYE
-				if (mousePoint.x > leftEye.x &&
-					leftEye.x <= leftEyeOG.x + eyeMovementRadius)
+				if (point.x > leftEye.x &&
+					leftEye.x <= eyeAnchor.x + eyeMovementRadius)
 				{
 					leftEye.x++;
 				}
-				if (mousePoint.x < leftEye.x &&
-					leftEye.x >= leftEyeOG.x - eyeMovementRadius)
+				if (point.x < leftEye.x &&
+					leftEye.x >= eyeAnchor.x - eyeMovementRadius)
 				{
 					leftEye.x--;
 				}
-				if (mousePoint.y > leftEye.y &&
-					leftEye.y <= leftEyeOG.y + eyeMovementRadius)
+				if (point.y > leftEye.y &&
+					leftEye.y <= eyeAnchor.y + eyeMovementRadius)
 				{
 					leftEye.y++;
 				}
-				if (mousePoint.y < leftEye.y &&
-					leftEye.y >= leftEyeOG.y - eyeMovementRadius)
+				if (point.y < leftEye.y &&
+					leftEye.y >= eyeAnchor.y - eyeMovementRadius)
 				{
 					leftEye.y--;
 				}
 
 			//RIGHT EYE
-				if (mousePoint.x > rightEye.x &&
-					rightEye.x <= rightEyeOG.x + eyeMovementRadius)
+				if (point.x > rightEye.x &&
+					rightEye.x <= eyeAnchor.z + eyeMovementRadius)
 				{
 					rightEye.x++;
 				}
-				if (mousePoint.x < rightEye.x &&
-					rightEye.x >= rightEyeOG.x - eyeMovementRadius)
+				if (point.x < rightEye.x &&
+					rightEye.x >= eyeAnchor.z - eyeMovementRadius)
 				{
 					rightEye.x--;
 				}
-				if (mousePoint.y > rightEye.y &&
-					rightEye.y <= rightEyeOG.y + eyeMovementRadius)
+				if (point.y > rightEye.y &&
+					rightEye.y <= eyeAnchor.w + eyeMovementRadius)
 				{
 					rightEye.y++;
 				}
-				if (mousePoint.y < rightEye.y &&
-					rightEye.y >= rightEyeOG.y - eyeMovementRadius)
+				if (point.y < rightEye.y &&
+					rightEye.y >= eyeAnchor.w - eyeMovementRadius)
 				{
 					rightEye.y--;
 				}
 		}
+		void ResetEyes() //Undos LookAtMouse
+		{
+			//LEFT EYE
+			if (leftEye.x < eyeAnchor.x)
+			{
+				leftEye.x++;
+			}
+			if (leftEye.x > eyeAnchor.x)
+			{
+				leftEye.x--;
+			}
+			if (leftEye.y < eyeAnchor.y)
+			{
+				leftEye.y++;
+			}
+			if (leftEye.y > eyeAnchor.y)
+			{
+				leftEye.y--;
+			}
+
+			//RIGHT EYE
+			if (rightEye.x < eyeAnchor.z)
+			{
+				rightEye.x++;
+			}
+			if (rightEye.x > eyeAnchor.z)
+			{
+				rightEye.x--;
+			}
+			if (rightEye.y < eyeAnchor.w)
+			{
+				rightEye.y++;
+			}
+			if (rightEye.y > eyeAnchor.w)
+			{
+				rightEye.y--;
+			}
+		}
+		void Move(Vector2 pos) //if false, end
+		{
+			//Move TOWARDS pos
+			if (position.x < pos.x && position.x + bodySize <= screenX)
+				position.x += speed.x;
+			if (position.x > pos.x && position.x - bodySize >= 0)
+				position.x -= speed.x;
+			if (position.y < pos.y && position.y + bodySize <= screenY)
+				position.y += speed.y;
+			if (position.y > pos.y && position.y - bodySize >= 0)
+				position.y -= speed.y;
+
+			body.x = position.x - bodySize / 2;
+			body.y = position.y - bodySize / 2;
+
+			if (tail.x < pos.x)
+			{
+				tail.x = body.x - tailSize + tailSize / 8;
+			}
+			else
+			{
+				tail.x = body.x + bodySize - tailSize / 8;
+			}
+			tail.y = body.y + bodySize - tailSize;
+
+			leftEar.x = body.x;
+			rightEar.x = body.x + bodySize - earSize.x;
+			leftEar.y = body.y - bodySize / 3 - earSize.y / 3;
+			rightEar.y = body.y - bodySize / 3 - earSize.y / 3;
+
+			eyeAnchor.x = body.x + bodySize / 2 - eyeSize / 2 - 25;
+			eyeAnchor.z = body.x + bodySize / 2 - eyeSize / 2 + 25;
+			eyeAnchor.y = body.y + bodySize / 3 + eyeSize / 2;
+			eyeAnchor.w = body.y + bodySize / 3 + eyeSize / 2;
+
+			if (leftEye.x < pos.x)
+			{
+				leftEye.x = eyeAnchor.x + 3;
+				rightEye.x = eyeAnchor.z + 3;
+			}
+			else
+			{
+				leftEye.x = eyeAnchor.x - 3;
+				rightEye.x = eyeAnchor.z - 3;
+			}
+			if (leftEye.y < pos.y)
+			{
+				leftEye.y = eyeAnchor.y + 3;
+				rightEye.y = eyeAnchor.w + 3;
+			}
+			else
+			{
+				leftEye.y = eyeAnchor.y - 3;
+				rightEye.y = eyeAnchor.w - 3;
+			}
+		}
 
 	}Pet;
 
-#pragma endregion
-
 	Pet pet;
 
+#pragma endregion
 
 	//feed pet, pickup box when mouse pressed on rect, shake food into bowl, pet approaches 10 seconds after last shake frame
 	//wash pet, pickup sponge when mouse pressed on rect, rub pet or dirty spots (lower opacity on enter) maybe hitbox smaller than square?
